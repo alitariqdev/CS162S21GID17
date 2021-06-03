38b2d5a8c296c48f78613ff05c1d32ffdafe3f00
alitariq12369 <alitariq.uet2020@gmail.com>
2021-06-03 18:12:48 +0500

    Payment class Functionality added

---
+++ RestaurantManagementSystem/RestaurantManagementSystem/Models/Payment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantManagementSystem.Models
+{
+    public class Payment
+    {
+
+
+
+    }
+}
