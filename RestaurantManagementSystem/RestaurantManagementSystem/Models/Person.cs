05d57626f3d257e86449ba530e2672f00e4e80ae
alitariq12369 <alitariq.uet2020@gmail.com>
2021-06-03 17:39:25 +0500

    Class Added

---
+++ RestaurantManagementSystem/RestaurantManagementSystem/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantManagementSystem.Models
+{
+    public class Person
+    {
+        public String FirstName { get; set; }
+        public String lastName { get; set; }
+        public String Contact { get; set; }
+        public String Email { get; set; }
+
+
+    }
+}
