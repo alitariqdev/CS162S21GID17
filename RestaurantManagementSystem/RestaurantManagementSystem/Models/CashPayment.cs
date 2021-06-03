6cc452055a7a40abe6d500b16f2c17c809aeccf4
hkb2001 <hasnainkbhatti@gmail.com>
2021-06-03 17:53:37 +0500

    classes added

---
+++ RestaurantManagementSystem/RestaurantManagementSystem/Models/CashPayment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantManagementSystem.Models
+{
+    public class CashPayment
+    {
+        public double Change { get; set; }
+    }
+}
